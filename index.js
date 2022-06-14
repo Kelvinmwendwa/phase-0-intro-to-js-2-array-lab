8875e61a948078f7705f00ab1c6d0f415ae2aea4
Kelvin <kmwendwa476@gmail.com>
2022-06-14 07:39:24 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+// cats
+const cats = ["Milo", "Otis", "Garfield"]
+
+//destructivelyAppendCat(name)
+function destructivelyAppendCat (name) {
+    cats.push(name);
+}
+
+//destructivelyPrependCat(name)
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+//destructivelyRemoveLastCat()
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+//destructivelyRemoveFirstCat()
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+//appendCat(name) - Spread operator
+function appendCat(name) {
+    return [...cats, name];
+}
+
+//prependCat(name) - Spread operator but at the beginning
+function prependCat (name) {
+    return [name, ...cats];
+}
+
+//removeLastCat
+function removeLastCat() {
+    return cats.slice(0,-1);
+}
+//removeFirstCat()
+function removeFirstCat() {
+    return cats.slice(1);
+}
